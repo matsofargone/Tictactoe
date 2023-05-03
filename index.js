bb2ddd1aaf9ae6d104569c8eb97848f2d3b1aaab
Mathew Webb <matsofargone@gmail.com>
2023-05-02 22:35:27 -0700

    setting up gameboard

---
+++ index.js
@@ -0,0 +1,28 @@
+//Module 
+
+var Gameboard = (function() {
+    var gameBoard = {'gameboard':[]};
+    
+    const getGameboard = ()=> {
+        return gameBoard.gameboard;
+    }
+
+    
+
+    return {getGameboard};
+ 
+    
+})();
+
+//factories 
+const Player = (name, mark) => {
+    
+}
+
+
+
+
+
+
+
+
