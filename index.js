a2ca243c630e7601fe3c94e051484f03707865a9
Mathew Webb <matsofargone@gmail.com>
2023-05-03 22:50:02 -0700

    gameboard created

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
 //Module 
 
 var Gameboard = (function() {
-    var gameBoard = {'gameboard':[]};
+    var gameBoard = {'gameboard':[" ", " ", " ", " ", " "," "," ", " ", " "]};
+    //going to handle getting and resetting the game board 
+    const container = document.getElementById("gameboard-container");
+
+    //create a table using  loops and assign each row 
     
-    const getGameboard = ()=> {
-        return gameBoard.gameboard;
+    var createGameboard = ()=> {
+        const gameboardTableElement = document.createElement("table");
+
+        for (let i = 0; i < 3; i++) {
+            const row = document.createElement("tr");
+
+    
+
+        for (let j = 0; j < 3; j++) {
+            const cell = document.createElement("td");
+            cell.classList.add('square');
+            row.appendChild(cell);
+        
     }
+    gameboardTableElement.appendChild(row);
+
+}
+    container.appendChild(gameboardTableElement);
+}
 
     
 
-    return {getGameboard};
+    return {createGameboard};
  
     
 })();
 
 //factories 
-const Player = (name, mark) => {
-    
+var Player = (name, mark) => {
+    const player = {'player_name': name,
+                    'mark': mark};
+
+    return {name, mark, player};
+
 }
 
 
+var GameLogic = (function() {
+
+    //code 
+    //Control flow to add game logic
+    //winning game logic
+
+})();
 
 
 
 
 
 
+
+
+Gameboard.createGameboard();
