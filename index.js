2e90665a0216bf7ab60eabb87021e6571b6fc85d
Mathew Webb <matsofargone@gmail.com>
2023-05-07 20:52:24 -0700

    added click event handling/ assigning index and slicing to gameboard

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ var Gameboard = (function() {
         for (let j = 0; j < 3; j++) {
             const cell = document.createElement("td");
             cell.classList.add('square');
+            //create index for the gameboard - will be used later to check win conditions
+            cell.dataset.row = i;
+            cell.dataset.col = j;
             row.appendChild(cell);
         
     }
@@ -27,28 +30,54 @@ var Gameboard = (function() {
     container.appendChild(gameboardTableElement);
 }
 
+    var setMarkerOnGameboard = () => {
+        container.addEventListener('click', function(e){
+            //convert indexs to integers 
+            const row = parseInt(e.target.dataset.row);
+            const col = parseInt(e.target.dataset.col);
+            //formula to create indexs 0-8
+            const index = (row * 3 + col);
+            //value set to players mark
+            const value = Player.mark;
+            //inserting the value into gameboard 
+            gameBoard.gameboard.splice(index, 1, value);
+            console.log(gameBoard.gameboard);
+            console.log(index);
+
+            //TODO: Handle changing square class to players mark
+        })
+    }
+
+    //TODO: Create resetBoard function to clear the board after game is finished may be created in GameLogic 
+
     
 
-    return {createGameboard};
+    return {createGameboard, setMarkerOnGameboard};
  
     
 })();
 
 //factories 
-var Player = (name, mark) => {
-    const player = {'player_name': name,
-                    'mark': mark};
+var Player = () => {
+    const player = {'player_name': 'Player 1',
+                    'mark': 'x'};
+    
 
-    return {name, mark, player};
+    return {player};
 
 }
 
 
 var GameLogic = (function() {
-
+    //TODO: Create logic for game 
+    //winning case
     //code 
     //Control flow to add game logic
     //winning game logic
+    Player.name = 'Player 1';
+    Player.mark = 'x';
+    Gameboard.createGameboard();
+    Gameboard.setMarkerOnGameboard();
 
 })();
 
@@ -58,5 +87,3 @@ var GameLogic = (function() {
 
 
 
-
-Gameboard.createGameboard();
