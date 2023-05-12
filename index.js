225ebbb775ad9274051c0948af640fd368b99bd3
Mathew Webb <matsofargone@gmail.com>
2023-05-11 21:57:34 -0700

    implemented condiitonal to protect double clicks

--- index.js
+++ index.js
@@ -1,85 +1,117 @@
-const gameboard = (() => {
-    var board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+function Gameboard (){
+    const board = [];
+   
     var container = document.getElementById("gameboard-container");
+    
+    var createBoard = () => {
 
-    var createBoard = () =>{
-        const boardTableElement = document.createElement("table");
-
+        const gameBoardTableElement = document.createElement('table');
         for (let i = 0; i < 3; i++) {
             const row = document.createElement("tr");
             
-
             for (let j = 0; j < 3; j++) {
                 const cell = document.createElement("td");
                 cell.classList.add('square');
-                //apply dataset numbers to each column and row 
-                //example - first cell - 0 - row 0 column index = 0
                 cell.dataset.row = i;
                 cell.dataset.col = j;
-                const index = (i * 3 + j);
-                cell.setAttribute('id', index);
+                
                 row.appendChild(cell);
-                // console.log(cell);
+                
             }
-            boardTableElement.appendChild(row);
+
+            gameBoardTableElement.appendChild(row);
+            
         }
-        container.appendChild(boardTableElement);
+
+            container.appendChild(gameBoardTableElement);
     }
 
-    var getBoard = () => {
-        return createBoard();
-    }
-
-    var getCellonBoard = (currentPlayer) =>{
-        
-          
-        
+    var setMarker = (mark) => {
         container.addEventListener('click', function(e){
-            const row = parseInt(e.target.dataset.row);
-            const col = parseInt(e.target.dataset.col);
-            //formula to create indexs 0-8
-            const index = (row * 3 + col);
-            board.splice(index, 1, currentPlayer.mark);
-            currentPlayer.setMarker(currentPlayer.mark, index);
-            console.log(index);
+            col = parseInt(e.target.dataset.col);
+            row = parseInt(e.target.dataset.row);
+            index = row * 3 + col;
+            if (board[index] !== board[" "]){
+                console.log('marked');
+                return;
+            }
+            board.splice(index, 1, mark);
+            
+            
+            
+            
+            
             console.log(board);
-            
-            
         })
     }
-   
-    return {getBoard , getCellonBoard}
-})();
 
 
-const player = (name, mark) =>{
-    const setMarker = (mark, index) => {
-        //get element 
-        var squareToMark = document.getElementById(index);
-        console.log(squareToMark);
-        squareToMark.innerHTML = mark;
-        
-    }
+    //grab fresh board
+    const getGameboard = () => createBoard();
 
-   
-
-return {name: name, mark: mark, setMarker:setMarker}
+        return {getGameboard, setMarker}
+    
 }
 
- 
-
-
-const displayController = (function() {
-    var player1 = player('Player 1', 'X');
-    var player2 = player('Player 2', 'O');
-
-    var currentPlayer = player1;
-
-    gameboard.getBoard();
-    currentPlayer = (currentPlayer === player1) ? player2 : player1;
-    console.log(gameboard.getCellonBoard(currentPlayer));
-
+function displayController() {
+    //code here
+    //handle DOM elements 
+    GameLogic();
     
     
-})();
+    
+}
+
+function Player(name,  mark = 'X') {
+   
+   
+    return {name: name, mark: mark}
+
+    
+}
+
+// function Player2(name, mark = 'O'){
+//     return {name: name, mark: mark}
+// }
+
+
+function GameLogic(){
+    //code here
+    //BackEnd Logic Try not to use DOM elements or front end things
+    var game = Gameboard();
+    game.getGameboard();
+   var player1 = Player('Player 1');
+   var player2 = Player('Player 2', 'O');
+   var players = [player1, player2];
+   var currentPlayer = players[0];
+    
+   
+   const switchTurns = () => {
+    //code here 
+    if (currentPlayer === player1){
+        return console.log(players[1]);
+    } else {
+        return console.log(players[0]);
+    }
+
+   }
+
+   
+   game.setMarker(currentPlayer.mark, currentPlayer);
+   
+   
+
+   
+   
+
+
+return {switchTurns:switchTurns}
+    
+   
+}
+
+
+displayController();
+
+
 
