67b52958696ee9643cf0c0b7cdfc8c3a99ae5a2d
Mathew Webb <matsofargone@gmail.com>
2023-05-15 19:14:07 -0700

    Winner logic implemented

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ const gameboard = (function() {
     //creating the game board
     const main = document.getElementById('main');
 
+   
+
+    
+
 
     const gameContainer = document.createElement('div');
     gameContainer.setAttribute('id', 'gameboard-container');
-   
+    
+    // var playerMoves = [];
+    // var cpuMoves = [];
 
     for (let i = 0; i < 9; i++) {
         let index = i;
@@ -30,31 +36,42 @@ const gameboard = (function() {
             var logic = gameLogic;
             var currentPlayer = logic.getCurrentPlayer();
             
+            // console.log(currentPlayer.name);
+          
             
-            
+           
             // console.log(currentPlayer);
             // console.log(currentPlayer);
-            
+           
             if (board[index] === "X" || board[index] === "O"){
                 console.log('taken');
                 return;
             }
+
+        
             // console.log(index);
             // console.log(cell);
             
-            board.splice(index, 1, currentPlayer.mark);
             
+            board.splice(index, 1, currentPlayer.mark);
+            console.log(logic.checkWinner());
+
+           
+          
            
 
             cell.innerHTML = currentPlayer.mark;
+            
             logic.setCurrentPlayer();
-           console.log(logic.getCurrentPlayer());
+            logic.getCurrentPlayer();
+           
+    
             
-            // console.log(currentPlayer);
+            
+           
             
             
-            
-            console.log(board);
+            // console.log(board);
             // console.log(getBoard(board));
 
         
@@ -64,61 +81,126 @@ const gameboard = (function() {
     };
 
     main.appendChild(gameContainer);
-    // console.log(board);
 
-    return {board,getBoard}
+    const disableBoard = () => {
+        gameContainer.style.pointerEvents = 'none';
+    }
+
+    return {board,getBoard, disableBoard}
 
 
 
 })();
 
 const gameLogic = (function() {
+    //* Function variables 
+    var xImage = new Image(150, 150);
+    xImage.classList.add('image');
+    xImage.src = './x.png';
     
+
     var player1 = player('Player 1', 'X');
     var player2 = player('Player 2', 'O');
     var updateBoard = gameboard.getBoard;
     var board = updateBoard(gameboard.board);
     var currentPlayer = player1;
-    console.log(currentPlayer);
-  
-    // console.log(board);
+    
+    
+   
+   //*Getting and setting the current Player
     const setCurrentPlayer = () => {
     currentPlayer === player1 ? currentPlayer = player2 : currentPlayer =  player1;
     // console.log('function ran');
     // console.log('current player:' + ' ' + currentPlayer.mark);
+    displayTurn();
+     
     }
 
     const getCurrentPlayer = () => {
+        
         return currentPlayer;
     }
-   
-  
+
+    
+    //*Creating div for player turns and calling the function so the player turn is displayed 
+    const turnsDiv = document.getElementById("turns");
+    var playerMessage = document.createElement('p');
+    turnsDiv.appendChild(playerMessage);
+    
+    const displayTurn = () => {
+        return playerMessage.textContent = currentPlayer.name + ' ' + 'turn';  
+     }
+
+     const displayWinner = () => {
+        
+        return playerMessage.textContent = currentPlayer.name + ' ' + 'is the Winner!';
+        
+     }
+
+     const displayTie = () => {
+        
+        return playerMessage.textContent = 'Draw!';
+        
+     }
+
+     
+// Display turn on load 
+
+    displayTurn();
+    
+//* Check winning conditions 
+    const winningConditions = [[0,1,2],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[0,4,8],[2,4,6],[6,7,8]];
+
+    const checkWinner = () => {
+        //code here
+        
+        
+        winningConditions.forEach((item) =>{
+            // console.log(item);
+        
+
+            if(board[item[0]] === currentPlayer.mark & board[item[1]] === currentPlayer.mark && board[item[2]] === currentPlayer.mark){
+               console.log(board[item[0]], board[item[1]], board[item[2]]);
+               if (item.length === 3){
+                console.log('winner');
+                console.log(item);
+                playerMessage = displayWinner();
+                gameboard.disableBoard();
+               } 
+                
+            } 
+
+           
+         });
+
+        
+         
+
+         
+        //After every turn I should check the gameboard array with the winning conditions and if it matches return the winner 
+        // also update player message to display winner 
+      
+
+    }
+
+    
+    
 
     
 
-    return { currentPlayer , board, setCurrentPlayer, getCurrentPlayer }
+    return { currentPlayer , setCurrentPlayer, getCurrentPlayer, displayTurn, checkWinner, displayWinner, displayTie }
 
 })();
 
 
 
 
-// const displayController = (function(){
-//     //code here will handle display elements
-//     // var board = gameboard.getBoard(gameboard.board);
-//     // var x = 'X';
-//     // var o = 'O';
-//     // let index = '';
+
+
+
+
     
-    
-//     // for (let i = 0; i < board; i++) {
-//     //     const cell = document.getElementById(i);
-//     //     if (cell === board[i]){
-//     //         cell.innerHTML = board[i];
-//     //         console.log(cell);
-//     //     }
-        
-//     // }
+
 
     
 
@@ -128,5 +210,5 @@ const gameLogic = (function() {
 
     
 
-// })();
+
 
